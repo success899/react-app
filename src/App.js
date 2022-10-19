7bb40c47d25b06ba19d2d1f801cc179f35610e40
success899 <success899@naver.com>
2022-10-19 18:31:36 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Header(){
+  return <header>
+  <h1><a href='/'>WEB</a></h1>
+</header>
+}
+
+function Nav(){
+  return <nav>
+  <ol>
+    <li><a href='/read/1'>HTML</a></li>
+    <li><a href='/read/2'>CSS</a></li>
+    <li><a href='/read/3'>JavaScript</a></li>
+  </ol>
+</nav>
+}
+
+function Article(){
+  return <article>
+  <h2>Welcome</h2>
+  Hello, WEB
+</article>
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        
+        <Nav></Nav>
+
+        <Article></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
